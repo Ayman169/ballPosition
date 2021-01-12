966fb593f12bd92da7d716863f02f96b8dbcd032
Sayeda Ayman <74244510+Ayman169@users.noreply.github.com>
2021-01-12 16:30:07 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,32 +1,52 @@
 var ball;
-
+var database,position;
 function setup(){
+    database=firebase.database();
+    console.log(database);
     createCanvas(600,600);
     
     ball=createSprite(200,400,30,30);
     ball.shapeColor="red";
+
+    var ballPosition=database.ref('ball/position');
+    ballPosition.on("value",readPosition,showError);
 }
 
 function draw(){
     background("lightblue");
 
-    if(keyDown("right")){
-        changePosition(2,0);
+    if(position !== undefined){   
+     if(keyDown("right")){
+        writePosition(2,0);
     }
    else if(keyDown("left")){
-        changePosition(-2,0);
+        writePosition(-2,0);
     }
    else if(keyDown("up")){
-        changePosition(0,-2);
+        writePosition(0,-2);
     }
    else if(keyDown("down")){
-        changePosition(0,2);
+        writePosition(0,2);
     }
 
     drawSprites();
 }
+}
 
-function changePosition(x,y){
-    ball.x=ball.x+x;
-    ball.y=ball.y+y;
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x': position.x+x,
+        'y': position.y+y
+    })
+}
+
+function readPosition(data){
+    position=data.val();
+    console.log(position.x);
+    ball.x=position.x;
+    ball.y=position.y;
+}
+
+function showError(){
+    console.log("Error");
 }
